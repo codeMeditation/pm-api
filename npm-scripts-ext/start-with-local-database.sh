c20c591c6c4da450acd53f1ee6c627d91c688307
Shankar <dharmarajan@jacob.de>
2023-05-08 21:22:31 +0200

    added base scaffolding

---
+++ npm-scripts-ext/start-with-local-database.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+which mysql
+if [ $? -ne 0 ]
+then
+  echo "Please intstall MySQL Client!"
+  exit 1
+fi
+
+docker-compose -f /docker-compose.yml -p pm-api up -d
+
+while ! mysql -u"root" -p"test-password" -h"localhost" --protocol=tcp -e"SELECT 1" &> /tmp/setup-local-dev-env
+do
+  sleep 1
+done
+
+mysql -u"root" -p"test-password" -h"localhost" --protocol=tcp -s < src/setup.sql &> /tmp/setup-local-dev-env
